ecf3d527d7a8e468791343d9cd3474e5d4f4ba48
Damian Ontivero <damian.ontivero@outlook.com>
2023-10-28 12:14:07 +0200

    Exception simple docstring

--- st_server/shared/application/exception.py
+++ st_server/shared/application/exception.py
@@ -2,32 +2,48 @@
 
 
 class NotFound(Exception):
+    """Raised when a resource is not found."""
+
     pass
 
 
 class AlreadyExists(Exception):
+    """Raised when a resource already exists."""
+
     pass
 
 
 class AuthenticationError(Exception):
+    """Raised when a user fails to authenticate."""
+
     pass
 
 
 class PasswordError(Exception):
+    """Raised when a password is invalid."""
+
     pass
 
 
 class PaginationError(Exception):
+    """Raised when pagination fails."""
+
     pass
 
 
 class FilterError(Exception):
+    """Raised when filtering fails."""
+
     pass
 
 
 class SortError(Exception):
+    """Raised when sorting fails."""
+
     pass
 
 
 class HandlerNotRegistered(Exception):
+    """Raised when a handler is not registered."""
+
     pass
